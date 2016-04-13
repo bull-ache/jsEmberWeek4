30326bc44ac8ea835342e7bda0b9be48830d785f
Blake Scott <blakerscott@gmail.com>
2016-04-12 17:36:00 -0700

    add content to new-answer.js

--- app/components/new-answer.js
+++ app/components/new-answer.js
@@ -1,4 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  answerCount: Ember.inject.service(),
+  addNewAnswer: false,
+  actions: {
+    answerFormShow() {
+      this.set('addNewAnswer', true);
+    },
+    save1() {
+      var params = {
+        content: this.get('content') ? this.get('content'): "",
+        author: this.get('author') ? this.get('author'): "",
+        question: this.get('question')
+      };
+      this.get('answerCount').add(params);
+      this.set('addNewAnswer', false);
+      this.sendAction('save2', params);
+    }
+  }
 });
