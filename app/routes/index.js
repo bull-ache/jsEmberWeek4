a17920da5f16cb7d59d0f57e3cdf51e664373e11
Blake Scott <blakerscott@gmail.com>
2016-04-12 17:54:14 -0700

    add content to routes

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,23 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return Ember.RSVP.hash({
+      questions: this.store.findAll('question'),
+      answers: this.store.findAll('answer')
+    });
+  },
+
+  actions: {
+    save3(params) {
+      var newQuestion = this.store.createRecord('question', params);
+      newQuestion.save();
+      this.transitionTo('index');
+    },
+    save5(newValue) {
+      var updateRating = this.store.updateRecord('question', params);
+      updateRating.save();
+      this.transitionTo('index');
+    }
+  }
 });
--- app/routes/question.js
+++ app/routes/question.js
@@ -1,4 +1,23 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  answerCount: Ember.inject.service(),
+
+  model(params) {
+    return this.store.findRecord('question', params.question_id);
+  },
+  actions: {
+    save3(params) {
+      var newAnswer = this.store.createRecord('answer', params);
+      console.log(params);
+      var question = params.question;
+      question.get('answers').addObject(newAnswer);
+      this.get('answerCount').add("1");
+      newAnswer.save().then(function() {
+        return question.save();
+      });
+      this.get('answerCount').add(params);
+      this.transitionTo('question', params.question);
+    }
+  }
 });
