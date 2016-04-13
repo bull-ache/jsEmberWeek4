740b74c390c829fd9b5d9ef56c4b65fceb0cd912
Blake Scott <blakerscott@gmail.com>
2016-04-12 17:52:29 -0700

    add content to models

--- app/models/answer.js
+++ app/models/answer.js
@@ -1,5 +1,7 @@
 import Model from 'ember-data/model';
 
 export default Model.extend({
-  
+  content: DS.attr(),
+  author: DS.attr(),
+  question: DS.belongsTo('question', { async: true }),  
 });
--- app/models/question.js
+++ app/models/question.js
@@ -1,5 +1,11 @@
 import Model from 'ember-data/model';
 
 export default Model.extend({
-  
+  content: DS.attr(),
+  author: DS.attr(),
+  notes: DS.attr(),
+  thumbsUp: DS.attr(),
+  thumbsDown: DS.attr(),
+  totalVotes: DS.attr(),
+  answers: DS.hasMany('answer', {async: true })
 });
