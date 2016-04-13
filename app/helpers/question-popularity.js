2ebe5f55afb5ade502c49d8c5d546a0c10bbc26d
Blake Scott <blakerscott@gmail.com>
2016-04-12 17:25:31 -0700

    add question-popularity helper

---
+++ app/helpers/question-popularity.js
@@ -0,0 +1,7 @@
+import Ember from 'ember';
+
+export function questionPopularity(params/*, hash*/) {
+  return params;
+}
+
+export default Ember.Helper.helper(questionPopularity);
---
+++ tests/unit/helpers/question-popularity-test.js
@@ -0,0 +1,10 @@
+import { questionPopularity } from 'js-ember-week4/helpers/question-popularity';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | question popularity');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = questionPopularity([42]);
+  assert.ok(result);
+});
