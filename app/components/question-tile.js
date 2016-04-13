5c0259ba665594ed0246e3adfd4a98ddaf866a64
Blake Scott <blakerscott@gmail.com>
2016-04-12 17:38:51 -0700

    add content to question-tile.js

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  areDetailsShowing: false,
+
+    actions: {
+      detailsShow: function() {
+        this.set('areDetailsShowing', true);
+      },
+      detailsHide: function() {
+        this.set('areDetailsShowing', false);
+      }
+    }
 });
