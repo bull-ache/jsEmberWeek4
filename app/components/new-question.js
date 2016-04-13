82a8c50e906e2f6fc06d75430ecf0a2a25ada205
Blake Scott <blakerscott@gmail.com>
2016-04-12 17:36:57 -0700

    add content to new-question.js

--- app/components/new-question.js
+++ app/components/new-question.js
@@ -1,4 +1,24 @@
 import Ember from 'ember';
 
+
 export default Ember.Component.extend({
+  addNewQuestion: false,
+  actions: {
+    questionFormShow() {
+      this.set('addNewQuestion', true);
+    },
+
+    save1() {
+      var params = {
+        content: this.get('content'),
+        author: this.get('author'),
+        notes: this.get('notes'),
+        thumbsUp: 0,
+        thumbsDown: 0,
+        totalVotes: 0,
+      };
+      this.set('addNewQuestion', false);
+      this.sendAction('save2', params);
+      }
+    }
 });
